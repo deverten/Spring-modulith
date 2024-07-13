b4bfe9e977402ce5370ff8739536ab416d5f9a46
Muhammed Umar <muumar@ecobank.com>
2024-07-13 21:24:03 +0100

    first commit for the spring modulith project

---
+++ src/test/java/org/mohammed/springmodulithcourse/SpringModulithCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mohammed.springmodulithcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringModulithCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
